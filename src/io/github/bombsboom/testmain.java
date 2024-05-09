4d6f703620f0dd2f497a885a86cdf52b02c643d2
Brandon Li <libr340849488@gmail.com>
2024-05-09 14:28:53 -0400

    test

--- src/io/github/bombsboom/testmain.java
+++ src/io/github/bombsboom/testmain.java
@@ -6,8 +6,7 @@ public class testmain extends JavaPlugin{
 
 	@Override
 	public void onEnable() {
-		getServer().getLogger().info("Loaded!");
-		getServer().getLogger().info("git push");
+		getServer().getLogger().info("override!");
 	}
 	
 }
